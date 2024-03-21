fd103917c0903600abd560fcd57d0a131c00fec9
carlos676767 <carlitobr730@gmail.com>
2024-03-20 17:21:46 -0700

    darkMode salvando no local storage

--- filmes/src/script.ts
+++ filmes/src/script.ts
@@ -2,12 +2,29 @@ const darkModeButton = document.getElementById("dark-mode-toggle") as HTMLInputE
 const bodyDarkMode = (classe1: string, classe2: string) => {
     document.body.classList.add(classe1);
     document.body.classList.remove(classe2);
+    return [classe1, classe2]
 }
+const setarDarkModeLocalStorage = (darkMode: string) => {
+  localStorage.setItem("modoEscuro", darkMode);
+}
+
 darkModeButton.addEventListener("change", () => {
+  const [darkMode, WhiteMode] = bodyDarkMode("dark-mode", "white-mode");
   if (darkModeButton.checked) {
-    bodyDarkMode("dark-mode","white-mode")
+    bodyDarkMode("dark-mode", "white-mode");
+    setarDarkModeLocalStorage(darkMode)
   } else {
-    bodyDarkMode("white-mode","dark-mode")
+    bodyDarkMode("white-mode", "dark-mode");
+    setarDarkModeLocalStorage(WhiteMode)
   }
 });
 
+const aplicarValoresSalvosDarkMode = () => {
+  const recuperarValor = localStorage.getItem("modoEscuro")
+  if (recuperarValor === "dark-mode") {
+    bodyDarkMode("dark-mode", "white-mode");
+  }else{
+    bodyDarkMode("white-mode", "dark-mode");
+  }
+}
+aplicarValoresSalvosDarkMode()
